57c568eb170cb3c9e0a65b9d62d634899ab271df
Hitoshi Nakashima <hitoshi.nakashima@gmail.com>
2018-01-04 12:14:38 +0900

    Add PIF file

--- main.go
+++ main.go
@@ -4,9 +4,28 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"syscall"
 )
 
 var port string
+
+func init() {
+	pidFilePath := os.Getenv("PIDFILE") || "go-gyazo.pid"
+	if ferr := os.Remove(pidFilePath); ferr != nil {
+		if !os.IsNotExist(ferr) {
+			panic(ferr.Error())
+		}
+	}
+	pidf, perr := os.OpenFile(pidFilePath, os.O_EXCL|os.O_CREATE|os.O_WRONLY, 0666)
+
+	if perr != nil {
+		panic(perr.Error())
+	}
+	if _, err := fmt.Fprint(pidf, syscall.Getpid()); err != nil {
+		panic(err.Error())
+	}
+	pidf.Close()
+}
 
 func main() {
 	envPort := os.Getenv("PORT")
